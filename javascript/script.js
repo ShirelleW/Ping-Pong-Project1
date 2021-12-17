ee339d2515a1240995febe358e3d4bf7d0f99bb1
ShirelleW <shirellewinecoffm@gmail.com>
2021-12-17 16:42:42 -0500

    Moving everything into Pong class

--- javascript/script.js
+++ javascript/script.js
@@ -11,6 +11,24 @@ class Rect{
         this.pos = new Vec;
         this.size = new Vec(w, h); 
     }
+
+    //getters
+    get left (){
+        //left end of rect
+        return this.pos.x - this.size.x / 2; 
+    }
+    get right (){
+        //right end of rect
+        return this.pos.x + this.size.x / 2; 
+    }
+        //top end of rect
+    get top(){
+        return this.pos.y - this.size.y / 2; 
+    }
+        //bottom end of rect
+    get bottom(){
+        return this.pos.y + this.size.y / 2 
+    }
 }
 //Create the ball
 class Ball extends Rect{
@@ -21,16 +39,51 @@ class Ball extends Rect{
     }
 }
 
-const canvas = document.getElementById("pong")
-const context = canvas.getContext("2d")
+class Pong{
+    constructor(canvas){
+        this._canvas = canvas;
+        this._context = canvas.getContext("2d")
 
-const ball = new Ball; 
-ball.pos.x = 100;
-ball.pos.y = 50; 
-// console.log(ball); 
+        this.ball = new Ball; 
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 50; 
+        this.ball.vel.x = 100; 
+        this.ball.vel.y = 100; 
 
-context.fillStyle = "#17F7F7"
-context.fillRect(0, 0, canvas.width, canvas.height)
 
-context.fillStyle = "#fff"
-context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y)
+        //begin to animate the ball, movement of ball is relative to the delta time of the update method
+        let lastTime;
+
+        const callback = (millis) => {
+            if (lastTime){
+                //convert to whole secs
+                this.update((millis - lastTime) / 1000); 
+            }
+            lastTime = millis; 
+            requestAnimationFrame(callback); 
+        };
+        callback();
+    }
+    update(dt){
+        this.ball.pos.x += this.ball.vel.x * dt;
+        this.ball.pos.y += this.ball.vel.y * dt; 
+    
+        //if ball hits canvas borders, reverse velocity
+        if (this.ball.left < 0 || this.ball.right > this._canvas.width){
+            this.ball.vel.x = -this.ball.vel.x;
+        }
+        if(this.ball.top < 0 || this.ball.bottom > this._canvas.height){
+            this.ball.vel.y = -this.ball.vel.y
+        }
+    
+       this._context.fillStyle = "#17F7F7"
+       this._context.fillRect(0, 0, this._canvas.width, this._canvas.height)
+    
+        //ball 
+       this._context.fillStyle = "#0145FB"
+       this._context.fillRect(this.ball.pos.x, this.ball.pos.y, this.ball.size.x, this.ball.size.y)
+    }
+}
+
+const canvas = document.getElementById("pong");
+const pong = new Pong(canvas);
