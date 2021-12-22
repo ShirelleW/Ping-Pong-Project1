d9f6700ab014306f29c2c316f6f3a61fd6a0c4f8
ShirelleW <shirellewinecoffm@gmail.com>
2021-12-22 12:56:41 -0500

    fourth commit: added score keep

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ first commit - getting canvas and ball to show uo in html page
 
 second commit - getting code into Pong class for easier implementation 
 
-third commit- Created players and implemented forEach loop-used to execute a function on each item in an array. 
+third commit- Created players and implemented forEach loop-used to execute a function on each item in an array.
+
+fourth commit- Added collison detection. Added keep score array. 
--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,15 @@ class Vec{
         this.x = x;
         this.y = y; 
     }
+    get len(){
+        //hypotenuses of triangle
+        return Math.sqrt(this.x * this.x + this.y * this.y )
+    }
+    set len(value){
+        const fact = value / this.len; 
+        this.x *= fact; 
+        this.y *= fact; 
+    }
 }
 // RECTANGLE
 class Rect{
@@ -39,6 +48,7 @@ class Ball extends Rect{
     }
 }
 
+// draw the paddles
 class Player extends Rect {
     constructor(){
         super(20, 100)
@@ -51,11 +61,8 @@ class Pong{
         this._canvas = canvas;
         this._context = canvas.getContext("2d")
 
-        this.ball = new Ball; 
-        this.ball.pos.x = 100;
-        this.ball.pos.y = 50; 
-        this.ball.vel.x = 100; 
-        this.ball.vel.y = 100; 
+        this.ball = new Ball;  
+
         
         //creating two players
         this.players = [
@@ -81,6 +88,43 @@ class Pong{
             requestAnimationFrame(callback); 
         };
         callback();
+
+        // score graphics
+        this.CHAR_PIXEL = 10; 
+        this.CHARS = 
+        [
+            "111101101101111", "010010010010010", "111001111100111", "111001111001111", "101101111001001", "111100111001111",
+            "111100111101111", "111001001001001", "111101111101111",
+            "111101111001111"
+        ].map(str => {
+            const canvas = document.createElement("canvas"); 
+            canvas.height = this.CHAR_PIXEL * 5
+            canvas.width = this.CHAR_PIXEL * 3
+            const context = canvas.getContext("2d")
+            context.fillStyle = "#FB1C29"
+            str.split("").forEach((fill, i) => {
+                if(fill === "1"){
+                    context.fillRect(
+                        (i % 3) * this.CHAR_PIXEL, 
+                        (i / 3 | 0) * this.CHAR_PIXEL, 
+                        this.CHAR_PIXEL,
+                        this.CHAR_PIXEL); 
+                }
+        })
+        return canvas;
+    })
+        this.reset();
+    }  
+    //collison detection
+    collide(player, ball){
+        if(player.left< ball.right && player.right > ball.left && player.top < ball.bottom && player.bottom > ball.top){
+            const len = ball.vel.len;
+            //negate ball velocity
+            ball.vel.x = -ball.vel.x
+            ball.vel.y += 300 * (Math.random () - .5)
+            //increase ball speed by 5% everytime it hits the ball paddles
+            ball.vel.len = len * 1.05  
+        }
     }
     draw(){
        this._context.fillStyle = "#17F7F7"
@@ -88,6 +132,8 @@ class Pong{
        
        this.drawRect(this.ball)
        this.players.forEach(player => this.drawRect(player))
+
+       this.drawScore(); 
     }
 
     drawRect(rect){
@@ -96,21 +142,67 @@ class Pong{
         this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y)
     }
 
+    //to string then back to integer
+    drawScore(){
+        const align = this._canvas.width / 3 
+        const CHAR_W = this.CHAR_PIXEL * 4 
+        this.players.forEach((player, index) => {
+            const chars = player.score.toString().split("");
+            const offset = align * (index + 1) - (CHAR_W * chars.length / 2) + 
+            this.CHAR_PIXEL /2; 
+
+            chars.forEach((char, pos) => {
+                this._context.drawImage(this.CHARS[char|0],
+                        offset + pos * CHAR_W, 20)
+            })
+        })
+    }
+
+    reset(){
+        this.ball.pos.x = this._canvas.width /2;
+        this.ball.pos.y = this._canvas.height / 2; 
+
+        this.ball.vel.x = 0; 
+        this.ball.vel.y = 0;
+    }
+
+    start(){
+        if(this.ball.vel.x === 0 && this.ball.vel.y === 0){
+        // tenerary op, if what is returned by math rand is > 5 , mult x valvue with 1 but if less tha n 5 , -1 vel
+        this.ball.vel.x = 300 * (Math.random() > .5 ? 1 : -1) 
+        this.ball.vel.y = 300 * (Math.random() * 2 - 1)
+        this.ball.vel.len = 200; 
+        }
+    }
+
     update(dt){
         this.ball.pos.x += this.ball.vel.x * dt;
         this.ball.pos.y += this.ball.vel.y * dt; 
     
         //if ball hits canvas borders, reverse velocity
+        //Keeping track of score
         if (this.ball.left < 0 || this.ball.right > this._canvas.width){
-            this.ball.vel.x = -this.ball.vel.x;
+            const playerId = this.ball.vel.x < 0 | 0; 
+            this.players[playerId].score++;
+            this.reset();
         }
         if(this.ball.top < 0 || this.ball.bottom > this._canvas.height){
             this.ball.vel.y = -this.ball.vel.y
         }
 
+        //this makes computer follow ball - very unfair AI 
+        this.players[1].pos.y = this.ball.pos.y
+
+        this.players.forEach(player => this.collide(player, this.ball))
         this.draw()
     }
 }
 
 const canvas = document.getElementById("pong");
 const pong = new Pong(canvas);
+
+// allow player 1 movement by mouse
+canvas.addEventListener("mousemove", event => pong.players[0].pos.y = event.offsetY)
+canvas.addEventListener("click", event => {
+    pong.start();
+}); 
